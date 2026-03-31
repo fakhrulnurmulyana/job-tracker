1de340a5c97b6c9aa18f7a21e6b37f9a53213c89
fakhrulnurmulyana <115722602+fakhrulnurmulyana@users.noreply.github.com>
2026-03-31 11:22:18 +0700

    chore: remove unused scrapper_services.py

--- src/job_tracker/orchestration/__init__.py
+++ src/job_tracker/orchestration/__init__.py
@@ -1,6 +1,9 @@
 from .job_pipeline_services import JobPipelineService
 from .job_processor import JobProcessor
 from .distribution_chart import DistributionChart
-from .scraper_service import ScraperOrchestrator
 
-__all__ = ["JobPipelineService", "JobProcessor", "DistributionChart", "ScraperOrchestrator"]
+__all__ = [
+    "JobPipelineService", 
+    "JobProcessor", 
+    "DistributionChart",
+]
